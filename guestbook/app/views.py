771260f8ac83db9a047b1963047762122565d4b4
Honza Horak <hhorak@redhat.com>
2015-10-10 23:01:18 +0200

    Change styling a bit

--- guestbook/app/views.py
+++ guestbook/app/views.py
@@ -3,17 +3,31 @@ from flask import request
 from flask import render_template
 from flask import redirect
 from flask import url_for
+import sqlalchemy
 import dataset
 import os
+import time
 
 # Connnect to database
 #db = dataset.connect('sqlite:///file.db')
-db = dataset.connect('postgresql://{username}:{password}@{host}:{port}/{dbname}'.format(
-                      host=os.environ.get('DB_PORT_5432_TCP_ADDR'),
-                      port=os.environ.get('DB_PORT_5432_TCP_PORT'),
-                      username=os.environ.get('DB_ENV_POSTGRESQL_USER'),
-                      password=os.environ.get('DB_ENV_POSTGRESQL_PASSWORD'),
-                      dbname=os.environ.get('DB_ENV_POSTGRESQL_DATABASE')))
+db = None
+tries = 0
+while not db and tries < 10:
+    try:
+        db = dataset.connect('postgresql://{username}:{password}@{host}:{port}/{dbname}'.format(
+                              host=os.environ.get('DB_PORT_5432_TCP_ADDR'),
+                              port=os.environ.get('DB_PORT_5432_TCP_PORT'),
+                              username=os.environ.get('DB_ENV_POSTGRESQL_USER'),
+                              password=os.environ.get('DB_ENV_POSTGRESQL_PASSWORD'),
+                              dbname=os.environ.get('DB_ENV_POSTGRESQL_DATABASE')))
+    except sqlalchemy.exc.OperationalError:
+        time.sleep(2)
+
+if not db:
+    print('Error: could not connect to DB: {host}:{port}'.format(
+          host=os.environ.get('DB_PORT_5432_TCP_ADDR'),
+          port=os.environ.get('DB_PORT_5432_TCP_PORT')))
+    exit(1)
 
 # create your guests table
 table = db['guests']
